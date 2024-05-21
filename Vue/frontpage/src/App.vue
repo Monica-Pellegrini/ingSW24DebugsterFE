67d76bd388ffc625c04a302153b769d039ad4e4e
Monica-Pellegrini <162020398+Monica-Pellegrini@users.noreply.github.com>
2024-05-21 18:19:52 +0200

    Add files via upload

--- Vue/frontpage/src/App.vue
+++ Vue/frontpage/src/App.vue
@@ -4,36 +4,40 @@
     <div id="hiddenKeys">
       <a accesskey="c" href="#main">vai al contenuto della pagina</a>
       <a accesskey="n" href="#search">vai al menu di navigazione</a>
-      <a accesskey="b" href="#btn_biglietteria"> vai alla sezione per comprare i biglietti</a>
+      <a accesskey="b" href="#BottoneBiglietteria"> vai alla sezione per comprare i biglietti</a>
       <a accesskey="a" href="/Portale/accessibilita.htm"> vai alla sezione Accessibilità</a>
       <a accesskey="p" href="#primo_link_footer">vai al pié di pagina</a>
     </div>
     <!--Inizio Header-->
     <header id="header">
-      <nav>
+      <nav role="navigation" aria-label="Barra Ricerca">
         <ul>
           <li class="searchbar">
             <!--Form per la ricerca-->
-            <form name="searchForm" method="get">
+            <form name="searchForm" role="form" method="get">
               <label></label>
-              <button type="submit" id="Bottonelente">
-                <img id="submit" src="@/images/lente.png" alt="Clicca qui per per cercare">
+              <button type="submit" role="button" id="Bottonelente">
+                <img id="submit" role="button" src="@/images/lente.png" alt="Clicca qui per per cercare">
               </button>
 
               <!--Textbox per ricerca-->
               <label></label>
-              <input type="text" id="search" name="search" value="" maxlength="50" required placeholder="Cerca . . ." size="50"/>
+              <input type="text" role="textbox" id="search" name="search" value="" maxlength="50" required placeholder="Cerca . . ." size="50"/>
             </form>
           </li>
+        </ul>
+      </nav>
+      <nav role="navigation" aria-label="Menù User">
+        <ul role="menu">
           <li>
             <!--Bottoni header-->
             <ul class="filtri">
-              <li><a class="BottoneHeader" href="filtri.html">Filtri</a></li>
+              <li role="menuitem"><a class="BottoneHeader" href="filtri.html">Filtri</a></li>
             </ul>
             <ul class="itemlist">
               <!--Filtri è vicino al textbox, gli altri sono raggruppati a sinistra-->
-              <li><a class="BottoneHeader" href="iscriviti.html">Iscriviti</a></li>
-              <li><a id="BottoneAccedi"  class="BottoneHeader" href="accedi.html">Accedi</a></li>
+              <li role="menuitem"><a class="BottoneHeader" href="iscriviti.html">Iscriviti</a></li>
+              <li role="menuitem"><a id="BottoneAccedi"  class="BottoneHeader" href="accedi.html">Accedi</a></li>
             </ul>
           </li>
         </ul>
@@ -41,8 +45,7 @@
     </header>
     <!--Fine Header-->
     <!--Inizio Main-->
-    <main id="main" class="clearfix">
-
+    <main id="main" class="clearfix" role="main">
       <!--Immagine principale-->
       <div id="box_banner">
         <carousel :slides="slides" :interval="3000" controls indicators id="banner"></carousel>
@@ -90,18 +93,18 @@
         </div>
 
         <!--Link vari-->
-        <nav id="NavDestra">
-          <ul>
+        <nav id="NavDestra" role="navigation">
+          <ul role="menu">
             <li>
               <ul class="LinkDestra">
-                <li><p class="BottoneDestra">Segui</p></li>
-                <li class="chat_live"><a class="BottoneDestra" href="chat.html">Chat</a></li>
-                <li class="chat_live"><a class="BottoneDestra" href="live.html">Live</a></li>
+                <li role="menuitem"><p class="BottoneDestra">Segui</p></li>
+                <li role="menuitem" class="chat_live"><a class="BottoneDestra" href="chat.html">Chat</a></li>
+                <li role="menuitem" class="chat_live"><a class="BottoneDestra" href="live.html">Live</a></li>
               </ul>
             </li>
-            <li id="BottoneRecensioni"><a class="BottoneDestra" href="recensioni.html">Recensioni</a></li>
-            <li id="BottoneSondaggiPre"><a class="BottoneDestra" href="http://localhost:8080/api/sondaggioPre">Sondaggio pre-evento</a></li>
-            <li id="BottoneSondaggiPost"><a class="BottoneDestra" href="http://localhost:8080/api/sondaggioPost">Sondaggio post-evento</a></li>
+            <li role="menuitem" id="BottoneRecensioni"><a class="BottoneDestra" href="recensioni.html">Recensioni</a></li>
+            <li role="menuitem" id="BottoneSondaggiPre"><a class="BottoneDestra" href="http://localhost:8080/api/sondaggioPre">Sondaggio pre-evento</a></li>
+            <li role="menuitem" id="BottoneSondaggiPost"><a class="BottoneDestra" href="http://localhost:8080/api/sondaggioPost">Sondaggio post-evento</a></li>
           </ul>
         </nav>
 
@@ -112,7 +115,7 @@
 
       <!--Link a inizio pagina-->
       <div id="torna_su">
-        <a href="#header"><img id="freccia" src="@/images/freccia.jpg" alt="Clicca qui per tornare in cima alla pagina"></a>
+        <a href="#header"><img id="freccia" role="button" src="@/images/freccia.jpg" alt="Clicca qui per tornare in cima alla pagina"></a>
       </div>
     </main>
     <!--Fine Main-->
@@ -120,23 +123,23 @@
     <footer>
       <!--Link da raggruppare in 3 gruppi da 3-->
       <!--Prima colonna-->
-      <nav class="footer-nav">
-        <ul class="prima_colonna">
-          <li id="primo_link_footer"><a href="home.html">Home Page</a></li>
-          <li><a href="faq.html">FAQs</a></li>
-          <li><a href="sponsor.html">Sponsorizzazioni</a></li>
+      <nav class="footer-nav" role="navigation" aria-label="link footer">
+        <ul class="prima_colonna" role="menu">
+          <li id="primo_link_footer" role="menuitem"><a href="home.html">Home Page</a></li>
+          <li role="menuitem"><a href="faq.html">FAQs</a></li>
+          <li role="menuitem"><a href="sponsor.html">Sponsorizzazioni</a></li>
         </ul>
         <!--Seconda colonna-->
-        <ul class="seconda_colonna">
-          <li><a href="user_sup.html">Supporto utente</a></li>
-          <li><a href="tec_sup.html">Supporto tecnico</a></li>
-          <li><a href="segnala.html">Segnala questo evento</a></li>
+        <ul class="seconda_colonna" role="menu">
+          <li role="menuitem"><a href="user_sup.html">Supporto utente</a></li>
+          <li role="menuitem"><a href="tec_sup.html">Supporto tecnico</a></li>
+          <li role="menuitem"><a href="segnala.html">Segnala questo evento</a></li>
         </ul>
         <!--Terza colonna-->
-        <ul class="terza_colonna">
-          <li><a href="privacy.html">Privacy</a></li>
-          <li> <a href="cookies.html">Cookies</a></li>
-          <li><a href="terms.html">Termini di utilizzo</a></li>
+        <ul class="terza_colonna" role="menu">
+          <li role="menuitem"><a href="privacy.html">Privacy</a></li>
+          <li role="menuitem"> <a href="cookies.html">Cookies</a></li>
+          <li role="menuitem"><a href="terms.html">Termini di utilizzo</a></li>
         </ul>
       </nav>
     </footer>
